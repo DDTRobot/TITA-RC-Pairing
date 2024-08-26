e6856b09a8a181538a2ab94a4854d0745f0976c3
wuyunzhou <wuyunzhou@directdrivetech.com>
2024-08-26 15:04:09 +0800

    fix:modify install path

--- install_yocto.sh
+++ install_yocto.sh
@@ -1,7 +1,7 @@
 make UART_SOURCE=0 
 cp crsf-app /usr/bin
-cp check-device.service /etc/systemd/system/
-cp check_device.sh /usr/bin
+cp usb-bind/check-device.service /etc/systemd/system/
+cp usb-bind/check_device.sh /usr/bin
 systemctl daemon-reload
 systemctl enable check-device.service
 systemctl restart check-device.service
