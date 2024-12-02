ff3fd426b0dc1c1beac35352742fba124e072dce
wuyunzhou <wuyunzhou@directdrivetech.com>
2024-12-02 15:41:47 +0800

    fix:add permission

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,6 +2,7 @@ make UART_SOURCE=1
 cp crsf-app /usr/bin
 cp usb-bind/check-device.service /etc/systemd/system/
 cp usb-bind/check_device.sh /usr/bin
+chmod 777 /usr/bin/check_device.sh
 systemctl daemon-reload
 systemctl enable check-device.service
 systemctl restart check-device.service
