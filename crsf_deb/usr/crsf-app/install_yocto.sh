201d0f36cd6de602b3caf3ee621b25456012449c
wuyunzhou <wuyunzhou@directdrivetech.com>
2025-07-04 18:34:26 +0800

    feat:version 1.1

--- crsf_deb/usr/crsf-app/install_yocto.sh
+++
@@ -1,7 +0,0 @@
-make UART_SOURCE=0 
-cp crsf-app /usr/bin
-cp usb-bind/check-device.service /etc/systemd/system/
-cp usb-bind/check_device.sh /usr/bin
-systemctl daemon-reload
-systemctl enable check-device.service
-systemctl restart check-device.service
